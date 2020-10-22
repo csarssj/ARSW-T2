8847a081bbd727e2842033432cfd02757ef26535
cesego <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-10-22 11:53:28 -0500

    starting spring

---
+++ src/main/java/edu/eci/arsw/weather/OpenWeatherApplication.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @author ceseg
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw.weather"})
+public class OpenWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWeatherApplication.class, args);
+    }
+}
